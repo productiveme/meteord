b058417e543a44bd3697d2a76de16a4b58c77f2c
Sudhanshu <sudhanshu@settlin.in>
2017-01-17 22:31:58 +0530

    canvas

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git
+apt-get install -y curl bzip2 build-essential python git libkrb5-dev imagemagick libcairo2-dev libjpeg-dev libpango1.0-dev libgif-dev g++ pkg-config
